d392a67cabb08c414dd75d7eb7aca9c3f7bfd4a0
jimmyahalpara <40048187+jimmyahalpara@users.noreply.github.com>
2018-06-08 08:23:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
-from distutils.core import setup
-setup(
-  name = 'Pytable',
-  packages = ['Pytable'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A module to create tables in python',
-  author = 'jimmy ahalpara',
-  author_email = 'jimmyahalpar123@gmail.com',
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
-)
+from setuptools import setup
+
+
+
+
+
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Internet',
+    'License :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    ]
+
+# calling the setup function 
+setup(name='Pytable',
+      version='1.0.0',
+      description='module for making tables in python',
+      long_description='module for making tables in python',
+      url='https://github.com/jimmyahalpara/Pytable',
+      author='Jimmy Kumar Ahalpara',
+      author_email='jimmyahalpara123@gmail.com',
+      license='MIT',
+      packages=['Pytable'],
+      classifiers=CLASSIFIERS,
+      keywords='tables'
+      )
