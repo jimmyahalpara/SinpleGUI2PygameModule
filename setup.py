28c2f4a4b2f7ff3d5b134aca144da90adfa73107
jimmyahalpara <40048187+jimmyahalpara@users.noreply.github.com>
2018-06-08 08:18:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'Pytable',
+  packages = ['Pytable'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A module to create tables in python',
+  author = 'jimmy ahalpara',
+  author_email = 'jimmyahalpar123@gmail.com',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
